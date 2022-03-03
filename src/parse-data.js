462229a2de806014174214716cc756a91ad3de39
smori <shinichiro.mori.1983@gmail.com>
2022-03-04 03:26:15 +0900

    Implement ParseResult and ParseData

---
+++ src/parse-data.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {Object} Data
+ * @property {string} defaultBranch
+ * @property {Object[]} actions
+ */
+
+class ParseData {
+  /**
+   * @param {Data} data
+   */
+  constructor(data) {
+    /**
+     * @type {ParseData}
+     * @private
+     */
+    this._data = data;
+  }
+
+  /**
+   * @returns {string}
+   */
+  getDefaultBranch() {
+    return this._data.defaultBranch;
+  }
+
+  /**
+   * @returns {Object[]}
+   */
+  getActions() {
+    return this._data.actions;
+  }
+}
+
+module.exports = ParseData;
