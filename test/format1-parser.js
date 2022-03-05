241b66d1baf44ca0cbf7c42628cc7e02e2b69f28
smori <shinichiro.mori.1983@gmail.com>
2022-03-05 16:03:23 +0900

    [test] Error thrown from ParseResult

--- test/format1-parser.js
+++ test/format1-parser.js
@@ -4,28 +4,48 @@ const assert = require('assert');
 const Parser = require('../src/format1-parser');
 
 describe('format1-parser', () => {
-  const okPatterns = require('./format1-parser_ok');
+  describe('ok pattern', () => {
+    const okPatterns = require('./format1-parser_ok');
 
-  given(okPatterns).it('should parse', (arg) => {
-    const parser = new Parser();
+    given(okPatterns).it('should parse', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
 
-    const parseResult = parser.parse(arg.input);
+      assert.deepStrictEqual(parseResult.parsed(), true);
+      assert.deepStrictEqual(parseResult.getParseData().dump(), arg.result);
+    });
 
-    assert.deepStrictEqual(parseResult.parsed(), true);
-    assert.deepStrictEqual(parseResult.getParseData().dump(), arg.result);
+    given(okPatterns).it('should throw error from getError()', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.throws(() => {
+        parseResult.getError();
+      }, Error);
+    });
   });
 
-  const errorPatterns = require('./format1-parser_error');
+  describe('error pattern', () => {
+    const errorPatterns = require('./format1-parser_error');
 
-  given(errorPatterns).it('should not parse', (arg) => {
-    const parser = new Parser();
+    given(errorPatterns).it('should not parse', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
 
-    const parseResult = parser.parse(arg.input);
+      assert.deepStrictEqual(parseResult.parsed(), false);
 
-    assert.deepStrictEqual(parseResult.parsed(), false);
+      const error = parseResult.getError();
 
-    const error = parseResult.getError();
+      assert.deepStrictEqual(error.location.start.line, arg.line);
+    });
 
-    assert.deepStrictEqual(error.location.start.line, arg.line);
+    given(errorPatterns).it('should throw error from getParseData()', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.throws(() => {
+        parseResult.getParseData();
+      }, Error);
+    });
   });
 });
