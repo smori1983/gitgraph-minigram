20aff55946c1dd80dbb939f17185bbd2ef389f1e
smori <shinichiro.mori.1983@gmail.com>
2022-03-30 00:25:16 +0900

    [test] format2-parser

---
+++ test/format2-parser.js
@@ -0,0 +1,53 @@
+const { describe } = require('mocha');
+const { given } = require('mocha-testdata');
+const assert = require('assert');
+const Parser = require('../src/format2-parser');
+
+describe('format2-parser', () => {
+  describe('ok pattern', () => {
+    const okPatterns = require('./format1-parser_ok');
+
+    given(okPatterns).it('should parse', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.deepStrictEqual(parseResult.parsed(), true);
+      assert.deepStrictEqual(parseResult.getParseData().dump(), arg.result);
+      assert.deepStrictEqual(parseResult.getParseData().getDefaultBranch(), arg.result.defaultBranch);
+      assert.deepStrictEqual(parseResult.getParseData().getActions(), arg.result.actions);
+    });
+
+    given(okPatterns).it('should throw error from getError()', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.throws(() => {
+        parseResult.getError();
+      }, Error);
+    });
+  });
+
+  describe('error pattern', () => {
+    const errorPatterns = require('./format1-parser_error');
+
+    given(errorPatterns).it('should not parse', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.deepStrictEqual(parseResult.parsed(), false);
+
+      const error = parseResult.getError();
+
+      assert.deepStrictEqual(error.location.start.line, arg.line);
+    });
+
+    given(errorPatterns).it('should throw error from getParseData()', (arg) => {
+      const parser = new Parser();
+      const parseResult = parser.parse(arg.input);
+
+      assert.throws(() => {
+        parseResult.getParseData();
+      }, Error);
+    });
+  });
+});
