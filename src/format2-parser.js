0b3bd60f03e34d96f7bf2f2e6871d21ca5c5e4da
smori <shinichiro.mori.1983@gmail.com>
2022-03-29 22:48:44 +0900

    Implement Format2Parser

---
+++ src/format2-parser.js
@@ -0,0 +1,49 @@
+const format2 = require('./grammar/format2');
+const LogManager = require('./grammar/log-manager');
+const ParseResult = require('./parse-result');
+const ParseData = require('./parse-data');
+
+class Format2Parser {
+  /**
+   * @param {string} input
+   * @returns {ParseResult}
+   */
+  parse(input) {
+    try {
+      const format2Parsed = format2.parse(input, {
+        logManager: new LogManager(),
+      });
+
+      const defaultBranch = this._resolveDefaultBranch(format2Parsed.option);
+      const actions = format2Parsed.log;
+
+      const parseData = new ParseData({
+        defaultBranch: defaultBranch,
+        actions: actions,
+      });
+
+      return new ParseResult(parseData, null);
+    } catch (e) {
+      return new ParseResult(null, e);
+    }
+  }
+
+  /**
+   * @param {Object[]} options
+   * @returns {string}
+   * @private
+   */
+  _resolveDefaultBranch(options) {
+    let result = 'master';
+
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].name === 'defaultBranch') {
+        result = options[i].value;
+      }
+    }
+
+    return result;
+  }
+}
+
+module.exports = Format2Parser;
